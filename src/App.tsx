6fe304cf612e5a8a3294352efbacebcade899431
nicolas <joulin.nicolas@gmail.com>
2019-11-29 22:27:50 +0100

    todo : up,down, right

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import Tile from './components/Tile'
 
-import { tileReducer } from './redux/reducer'
+import { tileReducer, spawnTile } from './redux/reducer'
 import './App.css'
 
+let tiles = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+tiles = spawnTile(tiles)
+tiles = spawnTile(tiles)
+
 const initialState = {
-  tiles: [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+  tiles: tiles,
+  score: 0
 }
 
 function App() {
   const [state, dispatch] = React.useReducer(tileReducer, {
     tiles: initialState.tiles,
+    score: initialState.score
   });
   function checkKey(e: KeyboardEvent) {
       if (e.keyCode === 38) {
         console.log('up')
+        dispatch({ type: 'MOVE_UP' })
       }
       else if (e.keyCode === 40) {
         console.log('down')
+        dispatch({ type: 'MOVE_DOWN' })
       }
       else if (e.keyCode === 37) {
         console.log('left')
+        dispatch({ type: 'MOVE_LEFT' })
       }
       else if (e.keyCode === 39) {
         console.log('right')
+        dispatch({ type: 'MOVE_RIGHT' })
       }
   }
 
@@ -47,6 +58,7 @@ function App() {
           ))
         }
       </div>
+      <div className="score">score : {state.score}</div>
     </div>
   )
 }
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,5 +1,8 @@
+import _ from 'lodash'
+
 interface TilesState {
   tiles: number[][]
+  score: number
 }
 
 type TilesAction = 
@@ -8,21 +11,69 @@ type TilesAction =
 | { type: 'MOVE_LEFT' }
 | { type: 'MOVE_RIGHT' }
 
+
+
 export function tileReducer(state: TilesState, action: TilesAction): TilesState {
   switch (action.type) {
     case 'MOVE_UP': 
-      return state 
-      break;
+      return state
+
     case 'MOVE_DOWN':
       return state
-      break;
+
     case 'MOVE_LEFT':
-      return state
-      break;
+        let newState = process(state.tiles, state.score)
+        return newState
+
     case 'MOVE_RIGHT':
       return state
-      break;
+
     default:
       return state
   }
+}
+
+const process = (tiles: number[][], score: number) => {
+  const initTiles = [...tiles]
+  for( let i = 0; i < tiles.length; i++){
+    let el = mergeRowCells(tiles[i])
+    tiles.splice(i,1, el.row)
+    score += el.score
+  }
+  //hack to compare init and modified array
+  if(JSON.stringify(tiles) === JSON.stringify(initTiles)){
+    return {tiles: tiles, score: score}
+  } else {
+    tiles = spawnTile(tiles)
+    return {tiles: tiles, score: score}
+  }
+}
+
+const mergeRowCells = (row: Array<number>) => {
+  let filteredRow = row.filter(value => value !== 0)
+  let score = 0
+  for(let i = 0; i < filteredRow.length; i++){
+    if(filteredRow[i] === filteredRow[i+1]){
+      let val = filteredRow[i]
+      filteredRow.splice(i, 1, val*2)
+      filteredRow.splice(i+1,1)
+      score += val*2
+    }
+  }
+  while(filteredRow.length < 4) filteredRow.push(0)
+  return {row: filteredRow, score: score}
+}
+
+export const spawnTile = (tiles: number[][]) => {
+  let tile = {
+    x: _.random(0,3),
+    y: _.random(0,3),
+    value: _.random(0,1,true) > 0.1 ? 2 : 4
+  }
+  while(tiles[tile.y][tile.x] !== 0){
+    tile.x = _.random(0,3)
+    tile.y = _.random(0,3)
+  }
+  tiles[tile.y][tile.x] = tile.value
+  return tiles
 }
